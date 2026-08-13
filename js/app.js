6ae87045e4ea427633704a2ba8779c9ed417d63a
ytxomik <ytxomik@gmail.com>
2026-08-13 11:29:15 +0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+// Auronex main script
 
+console.log("Auronex loaded");
+
+
+const cards = document.querySelectorAll(".card");
+
+
+cards.forEach((card,index)=>{
+
+    card.style.animation =
+    `show .8s ${index * 0.2}s both`;
+
+});
+
+
+// плавный эффект кнопки
+
+const button = document.querySelector("button");
+
+
+if(button){
+
+button.addEventListener("click",()=>{
+
+    document
+    .querySelector(".products")
+    .scrollIntoView({
+        behavior:"smooth"
+    });
+
+});
+
+}
