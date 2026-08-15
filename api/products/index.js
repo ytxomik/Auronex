828f7d995e7e17967031d871016e3397bb63c8b0
ytxomik <ytxomik@gmail.com>
2026-08-15 09:29:39 +0300

    Update index.js

--- api/products/index.js
+++ api/products/index.js
@@ -1,3 +1,4 @@
+
 import { neon } from "@neondatabase/serverless";
 
 const sql = neon(process.env.DATABASE_URL);
@@ -20,27 +21,30 @@ export default async function handler(req, res) {
         p.price,
         p.version,
         p.image_url,
+        p.download_url,
         p.active,
-        c.id AS category_id,
+        p.category_id,
         c.name AS category_name,
         c.slug AS category_slug
       FROM products p
       LEFT JOIN categories c
         ON c.id = p.category_id
-      WHERE p.active = TRUE
-      ORDER BY p.id DESC
+      WHERE p.active = true
+      ORDER BY p.id ASC
     `;
 
     return res.status(200).json({
       ok: true,
       products
     });
+
   } catch (error) {
-    console.error("GET /api/products error:", error);
+    console.error("PRODUCTS API ERROR:", error);
 
     return res.status(500).json({
       ok: false,
-      error: "Ошибка сервера."
+      error: "Не удалось загрузить товары."
     });
   }
 }
+
